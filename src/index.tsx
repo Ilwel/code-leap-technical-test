e37021e4975d276d18e8c16368218175b6c1e201
iueu <ilwelisaac@hotmail.com>
2023-04-13 23:02:12 -0300

    feat: index and router

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Root from './Root';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path:"/sign-up",
+        element: <SignUp/>
+      },
+      {
+        path: "/",
+        element: <Home/>
+      }
+    ]
+  }
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
